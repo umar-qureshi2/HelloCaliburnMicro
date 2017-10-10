b54aae0bbcc3c9659f9e193f516ffef8944dc6dc
umar-qureshi2 <umar.mcs@gmail.com>
2017-10-10 18:35:29 +0400

    new files

---
+++ UserFlow/AppBootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserFlow.ViewModels;
+using Caliburn.Micro;
+using System.Windows;
+
+namespace UserFlow
+{
+    public class AppBootstrapper : BootstrapperBase
+    {
+        public AppBootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            //base.OnStartup(sender, e);
+            DisplayRootViewFor<MainViewModel>();
+        }
+    }
+}
---
+++ UserFlow/ViewModels/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserFlow.ViewModels
+{
+    public class MainViewModel : Caliburn.Micro.PropertyChangedBase
+    {
+        private const string WindowTitleDefault = "Users Manager";
+
+        private string _windowTitle = WindowTitleDefault;
+
+        public string WindowTitle
+        {
+            get { return _windowTitle; }
+            set
+            {
+                _windowTitle = value;
+                NotifyOfPropertyChange(() => WindowTitle);
+            }
+        }
+
+    }
+}
